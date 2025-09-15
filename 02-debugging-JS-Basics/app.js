39de0745ee7934e3a9ef573a1360901cdb22289d
tris43210 <tristan.g.king@gmail.com>
2025-09-15 17:44:26 +0100

    basic debugging

---
+++ 02-debugging-JS-Basics/app.js
@@ -0,0 +1,11 @@
+console.log(`helloo world`);
+
+const myVariable = "yada yada yada";
+
+let init = 0;
+
+debugger; //Sets a manual breakpoint in the code - the code stops and the next lines can be checked through.
+while (init < 50) {
+  init++;
+  console.log(`this is iteration number ${init}`);
+}
